43d83d55fc260ffb6733d68f7d02617095f98e1d
Trinh Nhat <trinhminhnhatym@gmail.com>
2023-12-18 22:10:08 +0700

    update theme custom

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
 
 function App() {
 
-
   return (
     <>
+      <Typography variant="body2" color="text.secondary">test nè anh em</Typography>
       <div>trinh minh nhat ne</div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <AccessAlarmIcon/>
-      <ThreeDRotation/>
-      <HomeIcon />
       <HomeIcon color="primary" />
       <HomeIcon color="secondary" />
       <HomeIcon color="success" />
